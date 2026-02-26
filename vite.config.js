99ccfd740700b3e4772e14f5a844fcc604af5062
arCt1cX <92270123+arCt1cX@users.noreply.github.com>
2026-02-26 19:15:51 +0100

    bug fixes

--- vite.config.js
+++ vite.config.js
@@ -17,16 +17,16 @@ export default defineConfig({
         background_color: '#0a0a0f',
         display: 'standalone',
         orientation: 'any',
-        scope: '/',
-        start_url: '/',
+        scope: '/DrewPub/',
+        start_url: '/DrewPub/',
         icons: [
           {
-            src: '/icons/icon-192.png',
+            src: '/DrewPub/icons/icon-192.png',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: '/icons/icon-512.png',
+            src: '/DrewPub/icons/icon-512.png',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'any maskable'
